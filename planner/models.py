4eddcf9ed7f7660ae200a765416ef306627c042c
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-04-26 16:57:26 +0530

    Added start_time and duration

--- planner/models.py
+++ planner/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
 
 class Meeting(models.Model):
     title  = models.CharField(max_length=100)
     date = models.DateField()
-    
+    start_time = models.TimeField(default=datetime.now)
+    duration = models.IntegerField(default=15)
