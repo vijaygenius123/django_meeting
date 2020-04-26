023895a63a74d2da9ca7f4597a36f9a741b0fa44
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-04-26 18:15:14 +0530

    Added Simple Template

--- meeting/settings.py
+++ meeting/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
     'planner',
 ]
 
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'meeting.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
